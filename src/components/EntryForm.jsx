4c347c21d4ead79f74e89ff93f1dfa28f24fda2c
Nancy Hernandez <hernandezyanezhotmail.com>
2022-04-03 02:10:27 -0400

    Adding state and validation to the form

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -1,13 +1,34 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Row, Col, Alert } from "react-bootstrap";
 import useCategory from "../hooks/useCategory";
 
 const EntryForm = () => {
+  const [search, setSearch] = useState({
+    name: "",
+    category: "",
+  });
+
+  const [alert, setAlert] = useState("");
+
   const { categories } = useCategory();
 
-  console.log(categories);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(search).includes("")) {
+      setAlert("All fields are required");
+      return;
+    }
+    setAlert("");
+  };
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
+      {alert && (
+        <Alert variant="danger" className="text-center">
+          {alert}
+        </Alert>
+      )}
       <Row>
         <Col md={6}>
           <Form.Group className="mb-3 custom-text">
@@ -16,13 +37,30 @@ const EntryForm = () => {
               type="text"
               placeholder="Tequila, whisky, beer, etc."
               name="name"
+              value={search.name}
+              onChange={(e) =>
+                setSearch({
+                  ...search,
+                  [e.target.name]: e.target.value,
+                })
+              }
             />
           </Form.Group>
         </Col>
         <Col md={6}>
           <Form.Group className="mb-3 custom-text">
             <Form.Label htmlFor="category">Drink Category</Form.Label>
-            <Form.Select id="category" name="category">
+            <Form.Select
+              id="category"
+              name="category"
+              value={search.category}
+              onChange={(e) =>
+                setSearch({
+                  ...search,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            >
               <option>-Select Category-</option>
               {categories.map((item) => (
                 <option key={item.strCategory} value={item.strCategory}>
@@ -35,7 +73,11 @@ const EntryForm = () => {
       </Row>
       <Row className="justify-content-end">
         <Col md={3}>
-          <Button className="text-uppercase w-100 btn-custom" variant="custom">
+          <Button
+            className="text-uppercase w-100 btn-custom"
+            variant="custom"
+            type="submit"
+          >
             Search Drinks
           </Button>
         </Col>
