7fe521e559df391842ab494917345e1833416a27
Nancy Hernandez <hernandezyanezhotmail.com>
2022-04-02 23:35:36 -0300

    Creating  context to CategoryPovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Container } from "react-bootstrap";
 import EntryForm from "./components/EntryForm";
+import { CategoryProvider } from "./context/CategoryProvider";
 
 function App() {
   return (
-    <>
+    <CategoryProvider>
       <header className="py-4">
         <h1>🍹 Cocktail Browser</h1>
       </header>
       <Container className="mt-5">
         <EntryForm />
       </Container>
-    </>
+    </CategoryProvider>
   );
 }
 
---
+++ src/context/CategoryProvider.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, createContext } from "react";
+
+const CategoryContext = createContext();
+
+const CategoryProvider = ({ children }) => {
+  return (
+    <CategoryContext.Provider value={{}}>{children}</CategoryContext.Provider>
+  );
+};
+
+export { CategoryProvider };
+export default { CategoryContext };
